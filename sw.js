694e273cdf7f0cc86dd9db8fdfc685ba22322af2
wnsgj7519-ship-it <wnsgj7519@gmail.com>
2026-03-22 15:58:57 +0900

    sw.js 업데이트

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
-const CACHE_NAME = 'frog-fly-webapp-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener("install", event => {
+  event.waitUntil(self.skipWaiting());
 });
-self.addEventListener('activate', event => {
+
+self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
